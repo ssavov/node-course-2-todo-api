7f63ae870f9e1f909520bb6c6c3dcfd3fff8109c
Svetlio <svetlio@netreact.com>
2017-09-07 23:08:53 +0300

    Add DELETE /todos/:id route

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,16 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('59b1a293d1ff26c198bb578e').then((todo) => {
+    console.log(todo);
+});
