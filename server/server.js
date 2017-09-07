85f0cb4edbdd9dfa4815c50e603fddd5b2d3eae8
Svetlio <svetlio@netreact.com>
2017-09-07 23:39:38 +0300

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.delete('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send()
         }
-        return res.send(todo);
+        return res.send({todo});
     }).catch(e => res.status(400).send());
 
 });
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,42 @@ describe('GET /todos:id', () => {
             .end(done);
     })
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[0]._id.toHexString()
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Todo.findById(hexId)
+                    .then(todo => {
+                        expect(todo).toNotExist();
+                        done();
+                    })
+                    .catch((err) => done(err));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+            .delete(`/todos/${new ObjectID().toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 4040 for non-object ids', (done) => {
+        request(app)
+            .delete('/todos/1234')
+            .expect(404)
+            .end(done);
+    });
+});
