8c4e344534a2714a645f4116d77816c6e2148dd6
Svetlio <svetlio@netreact.com>
2017-09-12 02:13:45 +0300

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -112,6 +113,21 @@ app.post('/users', (req, res) => {
         .catch((e) => {
             res.status(400).send(e);
         })
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+
+    var token = req.header('x-auth');
+
+    User.findByToken(token).then(user => {
+        if (!user) {
+            return Promise.reject();
+        }
+        res.send(user);
+    }).catch((e) => {
+        res.status(401).send();
+    });
 });
 
 app.listen(port, () => {
