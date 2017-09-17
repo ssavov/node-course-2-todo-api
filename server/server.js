599bedd4012c765eb37aaaafb8426d849e878969
Svetlio <svetlio@netreact.com>
2017-09-18 02:10:22 +0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
+const bcrypt = require('bcryptjs');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -127,6 +128,22 @@ app.get('/users/me', authenticate, (req, res) => {
         res.send(user);
     }).catch((e) => {
         res.status(401).send();
+    });
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, [
+        'email',
+        'password'
+    ]);
+
+    User.findByCredentials(body.email, body.password)
+        .then((user) => {
+            return user.generateAuthToken().then((token) => {
+                res.header('x-auth', token).send(user);
+            })
+        }).catch((err) => {
+        res.status(400).send();
     });
 });
 
