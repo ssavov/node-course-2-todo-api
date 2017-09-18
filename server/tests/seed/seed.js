c2c1cc8273c3e56a885ebfde82533ec0e5825c62
Svetlio <svetlio@netreact.com>
2017-09-19 01:39:37 +0300

    Associate users with todos

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -18,17 +18,23 @@ const users = [{
 }, {
     _id: userTwoId,
     email: "mitka@example.com",
-    password: 'userTwoPass'
+    password: 'userTwoPass',
+    tokens: [{
+        access: 'auth',
+        token: jwt.sign({_id: userTwoId, access: 'auth'}, 'qwerty123').toString()
+    }]
 }];
 
 const todos = [{
     _id: new ObjectID(),
-    text: 'First test todo'
+    text: 'First test todo',
+    _creator: userOneId
 }, {
     _id: new ObjectID(),
     text: 'Second test todo',
     completed: true,
-    completed_at: 666
+    completed_at: 666,
+    _creator: userTwoId
 }];
 
 const populateTodos = (done) => {
